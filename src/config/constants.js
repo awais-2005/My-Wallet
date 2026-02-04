56103c7205c3c7203f848f110fc176985e2153c2
awais-2005 <rmawais852689@gmail.com>
2026-02-04 14:48:40 +0500

    constant for theme and other

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,4 @@
 export const SERVER_URL = "https://cloudflare-worker-backend.rmawais852689.workers.dev";
 export const API_KEY = "oE03kddUPJHtM3jYFO0eo3964XdXXVPR";
+
+export const CLOUDINARY_CLOUD_NAME="dh49op3h5"
---
+++ src/config/theme.js
@@ -0,0 +1,3 @@
+export const themeColor = "#368984";
+export const lowOpacityThmClr = "rgba(54, 137, 131, 0.79)";
+export const lightTheme = "#9ADBD2";
